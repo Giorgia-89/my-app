839749054268f7d7b0e986caf66ddd491456c090
root <root@LAPTOP-90DJQ2DE>
2023-10-11 15:40:11 +0100

    added more fields

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 
 @Component({
-  selector: 'form',
+  selector: 'formulario',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
   profile = new FormGroup({
-    fullName: new FormControl(''),
-    email: new FormControl(''),
+    fullName: new FormControl('',  [Validators.required]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    nif: new FormControl('', [Validators.required]),
   });
 }
+
+/**
+ * Validates a Portuguese taxpayer identification number (NIF).
+  @param {string} nif - The NIF to validate.
+  @returns {boolean} Returns true if the NIF is valid, false otherwise.
+
+export function validateNIF(nif: string) {
+  if (nif.length != 9) {
+    return false;
+  }
+  return true;
+} */
